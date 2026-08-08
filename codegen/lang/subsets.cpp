f0383724524c06f0718954ecdb956e06c7be04db
fractal <99148867+fractal-l@users.noreply.github.com>
2026-08-08 14:41:46 +0300

    fix: lang key subsets scanning for ayu_ prefixed keys

--- codegen/lang/subsets.cpp
+++ codegen/lang/subsets.cpp
@@ -193,10 +193,11 @@ void Scan(const QByteArray &content, Scanned &result) {
 	const auto data = content.constData();
 	const auto size = content.size();
 	for (auto i = qsizetype(0); i + 4 < size; ++i) {
-		if (data[i] != 'l'
-			|| data[i + 1] != 'n'
-			|| data[i + 2] != 'g'
-			|| data[i + 3] != '_'
+		const auto isKeyPrefix = (data[i] == 'l' && data[i + 1] == 'n'
+			&& data[i + 2] == 'g' && data[i + 3] == '_')
+			|| (data[i] == 'a' && data[i + 1] == 'y'
+				&& data[i + 2] == 'u' && data[i + 3] == '_');
+		if (!isKeyPrefix
 			|| (i > 0 && IsIdentifierChar(data[i - 1]))) {
 			continue;
 		}
